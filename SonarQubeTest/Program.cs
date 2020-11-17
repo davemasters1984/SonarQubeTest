3f37febbf402cd0582e24be6fb2d979ca8439803
Dave Masters <david.masters@vitality.co.uk>
2020-11-17 13:22:27 +0000

    Add project files.

---
+++ SonarQubeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SonarQubeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
