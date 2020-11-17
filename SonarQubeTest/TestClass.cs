65b0cd98ae5af12b6120afff5fd787ff0aad3aa6
Dave Masters <david.masters@vitality.co.uk>
2020-11-17 15:24:13 +0000

    test

---
+++ SonarQubeTest/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SonarQubeTest
+{
+    class TestClass
+    {
+        private string _one;
+        private string _two;
+        private string _three;
+        private string _four;
+        private string _five;
+
+        public TestClass(string one, string two, string three, string four, string five)
+        {
+            _one = one;
+            _two = two;
+            _three = three;
+            _four = four;
+            _five = five;
+        }
+    }
+}
