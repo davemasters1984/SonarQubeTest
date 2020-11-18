5fd9b9637f94e7a9f6b4326fd2f726af88cd6caf
Dave Masters <david.masters@vitality.co.uk>
2020-11-18 11:37:41 +0000

    another test

---
+++ src/SomeCode/ClassTwo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SomeCode
+{
+    public class ClassTwo
+    {
+        public string One { get; set; }
+        public string Two { get; set; }
+        public string Three { get; set; }
+        public string Four { get; set; }
+        public string Five { get; set; }
+
+        public ClassTwo(string one, string two, string three, string four, string five)
+        {
+            One = one;
+            Two = two;
+            Three = three;
+            Four = four;
+            Five = five;
+
+            //Test
+
+            if (five == null)
+            {
+                five.ToString();
+            }
+        }
+
+
+    }
+}
